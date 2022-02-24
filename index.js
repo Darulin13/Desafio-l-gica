6f038132e853e49ecceeff6ac5c05206c2d1e9b0
João Pedro <joaop338328@gmail.com>
2022-02-24 09:51:46 -0300

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+//01 - Estrutura de dados
+let name = "João"
+let number = 11
+let lastyear = 2022
+lastyear = lastyear-number
+result=lastyear+number
+console.log(result)
+console.log(`Estou mexendo no console.log `)
+let quartaFeira = true
+console.log( typeof quartaFeira)
+
+// 02 -  if/else
+let idade=19
+let humano=true
+let birthday="Janeiro"
+ if (idade > 18 && humano==true){
+    console.log("Você é humano")
+ }
+ if (birthday = "Janeiro" || "Dezembro"){
+    console.log("Parabéns")
+ }
+ //03 - for/while
+
+
+
+
